8621371415db2323df8646193346254a67584807
Paulius <paujak@gmail.com>
2021-01-12 13:27:12 +0200

    6iek tiek pradejo veikti

---
+++ paujak-spring/src/main/resources/public/precache-manifest.0728112a63a7509f4a652632769740ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f305d7e5de015811638479a8eec994f",
+    "url": "/paujak-spring/index.html"
+  },
+  {
+    "revision": "754cd50ab3169dce7156",
+    "url": "/paujak-spring/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "2de3f9587e5d3b8914e3",
+    "url": "/paujak-spring/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "754cd50ab3169dce7156",
+    "url": "/paujak-spring/static/js/2.4143788a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/paujak-spring/static/js/2.4143788a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea71629682c53e9b3880",
+    "url": "/paujak-spring/static/js/3.bab6ce62.chunk.js"
+  },
+  {
+    "revision": "2de3f9587e5d3b8914e3",
+    "url": "/paujak-spring/static/js/main.be325333.chunk.js"
+  },
+  {
+    "revision": "892fa33aaa68576c6e7e",
+    "url": "/paujak-spring/static/js/runtime-main.0e95bd9c.js"
+  }
+]);
--- paujak-spring/src/main/resources/public/service-worker.js
+++ paujak-spring/src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paujak-react/precache-manifest.3d61d93667ad91f2c2874cc47be1e330.js"
+  "/paujak-spring/precache-manifest.0728112a63a7509f4a652632769740ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paujak-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paujak-spring/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

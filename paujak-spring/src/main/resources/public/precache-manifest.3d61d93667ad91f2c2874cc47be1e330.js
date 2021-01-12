03b47625601381876508a1ecff00ca3a3f1ade37
Paulius <paujak@gmail.com>
2021-01-12 13:11:35 +0200

    sudetas react'o buildas

---
+++ paujak-spring/src/main/resources/public/precache-manifest.3d61d93667ad91f2c2874cc47be1e330.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29cd3f4c623149b4358ff525c153938",
+    "url": "/paujak-react/index.html"
+  },
+  {
+    "revision": "754cd50ab3169dce7156",
+    "url": "/paujak-react/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "46093fdd3e50632627d2",
+    "url": "/paujak-react/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "754cd50ab3169dce7156",
+    "url": "/paujak-react/static/js/2.4143788a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/paujak-react/static/js/2.4143788a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea71629682c53e9b3880",
+    "url": "/paujak-react/static/js/3.bab6ce62.chunk.js"
+  },
+  {
+    "revision": "46093fdd3e50632627d2",
+    "url": "/paujak-react/static/js/main.ed6883e2.chunk.js"
+  },
+  {
+    "revision": "8188302e0d168153bb24",
+    "url": "/paujak-react/static/js/runtime-main.e2ac8309.js"
+  }
+]);
